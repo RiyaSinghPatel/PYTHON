62041fa915e6b31b082f88d17b43421468c38c40
Riya Singh Patel <SinghRiyaPatel3@gmail.com>
2024-01-29 23:22:03 -0500

    Basic if else pratoce

--- Python Book Practice.py
+++ Python Book Practice.py
@@ -80,4 +80,109 @@ s3=s1//s2
 print(f"{s1} is", s3, f"times of {s2}")
 
 
+#**************************************BASIC - iF-ELSE QUESTION***********************************************
 
+# 012 Ask for two numbers. If the first one is larger than the second, display the second number first and then the first 
+# number, otherwise show the first number first and then the second. 
+
+
+n1= int(input("enter the first number:"))
+n2= int(input("enter the secound number:"))
+if n1<n2:
+    print(f"{n2} is greater than {n1}")
+else:
+     print(f"{n1} is greater than {n2}")
+
+# 013 Ask the user to enter a number that is under 20. If they enter a number that is 20 or more, display the message “Too high”, 
+# otherwise display “Thank you”.
+     
+n1= int(input("enter the number:"))
+if n1 < 20:
+    print("thankyou!")
+elif n1==20 or n1>20:
+         print("too high!")
+
+
+# 14 Ask the user to enter a number between 10 and 20 (inclusive). If they enter a number within this range, display the message “Thank 
+# you”, otherwise display the message “Incorrect answer”. 
+         
+n1= int(input("enter the number between 10 and 20:"))
+if n1>= 10 and n1 <= 20:
+      print("thankyou")
+else:
+      print("incorrect ans")
+
+# 15 Ask the user to enter their favourite colour. If they enter “red”, “RED” or “Red” display the message “I like red too”,
+# otherwise display the message “I don’t like [colour], I prefer red”. 
+      
+n1=input("enter the color:")
+
+if n1.lower() == "red":
+    print(f"I like this color {n1.lower()}")
+else:
+    print(f"I dont like this {n1} color")
+
+# 016 Ask the user if it is raining and convert their answer to lower case so it doesn’t matter what case they type it in. 
+# If they answer “yes”, ask if it is windy. If they answer “yes” to this second question, display the answer “It is too windy 
+# for an umbrella”, otherwise display the message “Take an umbrella”. If they did not answer yes to the first question,
+# display the answer “Enjoy your day”. 
+
+
+
+n1=input("if it is raining")
+n1_l = n1.lower()
+if n1_l == "yes":
+     n2=input("is it windy:")
+     n2_l = n2.lower()
+     if n2_l == "yes":
+          print("It is too windy for an umbrella")
+     else:
+          print("Take an umbrella")
+else:
+     print("enjoy your day")
+
+
+# 17 Ask the user’s age. If they are 18 or over, display the message “You can vote”, if they are aged 17, display the 
+# message “You can learn to drive”, if they are 16, display the message “You can buy a lottery ticket”, if they are 
+# under 16, display the message “You can go Trickor-Treating”.
+
+age = int(input("Please enter your age: "))
+
+if age >= 18:
+    print("You can vote.")
+elif age == 17:
+    print("You can learn to drive.")
+elif age == 16:
+    print("You can buy a lottery ticket.")
+else:
+    print("You can go trick-or-treating.")
+
+
+# 018 Ask the user to enter a number. If it is under 10, display the message “Too low”, if their number is 
+# between 10 and 20, display “Correct”, otherwise display “Too high”
+    
+num = int(input("Please enter number: "))
+if num<10:
+     print("too low")
+elif num > 10 and num < 20:
+     print("correct")
+else:
+     print("too high")
+
+# 19 Ask the user to enter 1, 2 or 3. If they enter a 1, display the message “Thank you”, if they enter a 2, display 
+# “Well done”, if they enter a 3, display “Correct”. If they enter anything else, display “Error message”.
+     
+
+num = int(input("Please enter number: "))
+
+if num == 1:
+    print("thankyou")
+elif num == 2:
+    print("Well done")
+elif num == 3:
+    print("correct")
+else:
+    print("error messsage")
+
+
+#***************************************BASIC - STRING question***********************************
